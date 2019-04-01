e23fdb373fcd64f9f6cde21b5872179e4f1c811b
Alex Zaharia <alecszaharia@gmail.com>
2019-04-01 11:47:49 +0300

    White label fixes and changed

--- admin/settings.php
+++ admin/settings.php
@@ -81,7 +81,7 @@ class Brizy_Admin_Settings {
 			'read',
 			self::menu_slug(),
 			array( $this, 'render' ),
-			__bt( 'brizy-logo', plugins_url( '../admin/static/img/brizy.png', __FILE__ ) ),
+			__bt( 'brizy-logo', plugins_url( 'static/img/brizy.png', __FILE__ ) ),
 			//plugins_url( '/static/img/brizy-logo.svg', __FILE__ ),
 			81
 		);
